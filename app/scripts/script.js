4af85137e21ec3fbdd87c617d27c6c88bcf0225b
Михаил Никонов <m.nikonov@gradoservice.ru>
2015-09-10 20:00:13 +0300

    dirty version

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
-    $(".height").css("height",$(window).outerHeight());
+    $(".top-menu .col-md-2 a").on('click',function(event){
+        $(this).parent().parent().find('.col-md-2').removeClass('active');
+        $(this).parent().addClass('active');
+    })
 });
 
-$(document).resize(function(){
-    $(".height").css("height",$(window).outerHeight());
+$(window).on('scroll',function(){
+    $('.block').each(function(){
+        if($(window).scrollTop() >= $(this).offset().top){
+            console.log(123);
+            var id = $(this).attr('id');
+            $('.top-menu .col-md-2').removeClass('active');
+            $('.top-menu .col-md-2 a[href=#'+id+']').parent().addClass("active");
+        }
+    });
 });
