0806fc5c4b057b633b7900be9b50c145d45ab657
Mike Nikonov <lounge1k@gmail.com>
2016-05-23 01:48:36 +0300

    last commit

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,3 +1,39 @@
 $(document).ready(function(){
 
+    $('.top-menu a').on('click', function() {
+        console.log(this);
+        var scrollAnchor = $(this).attr('data-scroll'),
+            scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top -20;;
+
+
+        $('body,html').animate({
+            scrollTop: scrollPoint
+        }, 500);
+
+        return false;
+
+    });
+
 });
+$(window).scroll(function() {
+    var windscroll = $(window).scrollTop();
+    if (windscroll >= 100) {
+        $('.top-menu').addClass('fixed');
+        $('.wrap section').each(function(i) {
+            if ($(this).position().top <= windscroll+20) {
+                $('.top-menu li.active').removeClass('active');
+                $('.top-menu li').eq(i).addClass('active');
+            }
+        });
+
+    } else {
+
+        $('.top-menu').removeClass('fixed');
+        $('.top-menu li.active').removeClass('active');
+        $('.top-menu li:first').addClass('active');
+    }
+
+}).scroll();
+var show = function(){
+    $('#myModal').modal('show');
+}
