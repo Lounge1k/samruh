de1690e0f86b90ac7ae02807188f3549d7e4573e
Mike Nikonov <lounge1k@gmail.com>
2016-11-04 05:53:54 +0300

    trying to gulp

--- gulp/buld.js
+++ gulp/buld.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var gutil = require('gulp-load-utils')(['log']);
+var inject = require('gulp-inject');
 var $ = require('gulp-load-plugins')();
 
 var handleError = function (err) {
@@ -9,6 +10,15 @@ var handleError = function (err) {
   console.log(err.getStack());
   process.exit(1);
 };
+
+gulp.task('index', function () {
+  var target = gulp.src('./index.html');
+  // It's not necessary to read the files (will speed up things), we're only after their paths:
+  var sources = gulp.src(['./app/scripts/*.js', './app/less/*.css'], {read: false});
+
+  return target.pipe(inject(sources))
+      .pipe(gulp.dest('./'));
+});
 
 gulp.task('styles', function() {
 
@@ -33,4 +43,4 @@ gulp.task('clean', function () {
   return gulp.src(['.tmp', 'dist'], { read: false }).pipe($.clean());
 });
 
-gulp.task('build', ['styles']);
+gulp.task('build', ['styles','index']);
