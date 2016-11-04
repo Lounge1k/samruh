e776fc52f77017385146c4c5355a595f014c9603
Lounge1k <lounge1k@gmail.com>
2016-11-04 18:55:44 +0300

    main bug fixes

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -15,7 +15,7 @@ function init(){
 
     myPlacemark = new ymaps.Placemark([55.79532166197225,49.127950099584496], {
         hintContent: 'Samruh',
-        balloonContentHeader:'<p style="color:#583305;font-size:14px;">"SAMRUH" Art Association</p>',
+        balloonContentHeader:'<p style="color:#583305;font-size:14px;">"SAMRUH" Artist Association</p>',
         balloonContentBody: '<p>ул Карла Маркса, 46 б.</p><p>+7 916 155 86 94</p><p>mail@samruh.com</p>'
     });
 
